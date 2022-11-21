4925998309476599b59f62a04c6cef8ab7cdf4e4
Matias Lopez <mati021796@gmail.com>
2022-11-21 19:02:40 -0300

    guardo por las dudas

---
+++ carrito.js
@@ -0,0 +1,229 @@
+class Carrito {
+    
+    static crearHTML() {
+        let modal = document.querySelector(".modal");
+        let fondoModal = document.querySelector(".modal-backdrop");
+        if(!modal&&!fondoModal){
+            /* Fondo modal */
+            let fondoModal = document.createElement("div");
+            fondoModal.classList.add(...["modal-backdrop", "fade", "show"]);
+            document.body.appendChild(fondoModal);
+
+            /* Creacion de elementos para el modal */
+            let modal = document.createElement("div");
+            let modal_dialog = document.createElement("div");
+            let modal_content = document.createElement("div");
+            let modal_header = document.createElement("div");
+            let h4 = document.createElement("h4");
+            let boton = document.createElement("button");
+            let modal_body = document.createElement("div");
+            let modal_footer = document.createElement("div");
+            let boton_cerrar = document.createElement("button");
+
+            /* Appends */
+            document.body.appendChild(modal);
+            modal.appendChild(modal_dialog);
+            modal_dialog.appendChild(modal_content);
+            modal_content.appendChild(modal_header);
+            modal_content.appendChild(modal_body);
+            modal_content.appendChild(modal_footer);
+            modal_header.appendChild(h4);
+            modal_header.appendChild(boton);
+            modal_footer.appendChild(boton_cerrar);
+
+            /* Informacion */
+            h4.innerHTML = "Carrito";
+            boton_cerrar.innerHTML = "Cerrar";
+
+            /* Se agregan las clases y atributos de Bootstrap */
+            modal.classList.add("modal");
+            modal.id = "exampleModalScrollable";
+            modal_dialog.classList.add(...["modal-dialog", "modal-dialog-scrollable"]);
+            modal_content.classList.add(...["modal-content", "bg-customize"]);
+            modal_header.classList.add("modal-header");
+            h4.classList.add(...["modal-title", "text-center"]);
+            boton.classList.add("btn-close");
+            boton.setAttribute("type", "button");
+            modal_body.classList.add("modal-body");
+            modal_footer.classList.add("modal-footer");
+            boton_cerrar.classList.add(...["btn", "btn-lg", "btn-success", "text-light"]);
+            boton_cerrar.setAttribute("data-cerrar", true);
+        }
+    }
+
+    static abrirCarrito(){
+        let modalBackdrop = document.querySelector(".modal-backdrop");
+        let modal = document.querySelector(".modal");
+        /* Animacion */
+        modal.classList.add("transicionModal");
+        modal.style.display = "block";
+        modalBackdrop.style.display="block";
+    }
+
+    static cerrarCarrito(e){
+        /* Animacion */
+        let padre = e.target.parentElement.parentElement.parentElement.parentElement;
+        padre.previousElementSibling.classList.add("transicionInversaFondo");
+        padre.classList.add("transicionInversa");
+        // Elimino los elementos html para que no carguen la pagina por si se ejecutan varias veces, etc
+        setTimeout(() => {
+            padre.previousElementSibling.classList.remove("transicionInversaFondo");
+            padre.classList.remove("transicionInversa");
+            padre.previousElementSibling.style = "display:none;";
+            padre.style = "display:none;";
+        }, 1000);
+    }
+
+    static mostrarProductos() {
+        let carrito = JSON.parse(localStorage.getItem("carrito"));
+        let modalBody = document.querySelector(".modal-body");
+        if(carrito){
+            for(let i = 0; i<carrito.length;i++){
+                let div = document.createElement("div");
+                modalBody.appendChild(div);
+                div.classList.add(...["items"]);
+                div.setAttribute("data-id-item", i);
+                let img = document.createElement("img");
+                let h5 = document.createElement("h5");
+                let span = document.createElement("span");
+                let divContenedorInput = document.createElement("div");
+                let decrementador = document.createElement("button");
+                let input = document.createElement("input");
+                let incrementador = document.createElement("button");
+                div.appendChild(img);
+                div.appendChild(h5);
+                div.appendChild(span);
+                div.appendChild(divContenedorInput);
+                divContenedorInput.appendChild(decrementador);
+                divContenedorInput.appendChild(input);
+                divContenedorInput.appendChild(incrementador);
+                decrementador.setAttribute("data-name", "decrementador");
+                incrementador.setAttribute("data-name", "incrementador");
+                decrementador.textContent="-";
+                incrementador.textContent="+";
+                img.style="width:20%";
+                h5.textContent=carrito[i].nombre;
+                img.src=carrito[i].imagenes[0];
+                span.textContent=carrito[i].precio;
+                input.value=carrito[i].cantidad;
+                input.type="number";
+            }
+        } else {
+            let p = document.createElement("p");
+            modalBody.appendChild(p);
+            p.textContent="No hay productos agregados al carrito";
+        }
+    }
+
+    static vacioProductos(){
+        let modalBody = document.querySelector(".modal-body");
+
+        document.querySelectorAll(".modal-body .items")?.forEach(e=>{
+        modalBody.removeChild(e);
+        });
+
+        document.querySelectorAll(".modal-body p")?.forEach(e=>{
+            modalBody.removeChild(e);
+        });
+
+    }
+
+    static incrementador(e){
+
+    }
+
+    static decrementador(e){
+        let padre = e.target.parentElement.parentElement;
+        let local = JSON.parse(localStorage.getItem("carrito"));
+        let id = padre.getAttribute("data-id-item");
+        let producto = local.splice(local[id], 1);
+        console.log(producto);
+        if(producto.cantidad>1){
+            producto.cantidad--;
+            
+        } else {
+            if(confirm("No puede tener menos de un item, ¿estas seguro de que quieres eliminarlo?")){
+                let modalBody = padre.parentElement;
+                modalBody.removeChild(padre);
+            } else {
+                alert("No se ha eliminado");
+            }
+        }
+    }
+
+    producto;
+
+    constructor(producto) {
+        this.producto = producto;
+        let carrito = JSON.parse(localStorage.getItem("carrito"));
+        if (carrito) {
+            for (let j = 0; j < carrito.length; j++) {
+                if (carrito[j].nombre == producto.nombre) {
+                    var validador = true;
+                    var carritoIndex = j;
+                    break;
+                }
+            }
+
+            if (!validador) {
+                this.producto.cantidad = 1;
+                carrito.push(this.producto);
+                localStorage.setItem("carrito", JSON.stringify(carrito));
+                return;
+            }
+
+            var productoCarrito = carrito.splice(carritoIndex, 1)[0];
+
+            productoCarrito.cantidad++;
+
+            carrito.push(productoCarrito);
+            localStorage.setItem("carrito", JSON.stringify(carrito));
+        } else {
+            this.producto.cantidad = 1;
+            localStorage.setItem("carrito", JSON.stringify([this.producto]));
+        }
+    }
+
+    mensajeCarrito(i){
+        let toast = document.createElement("div");
+        let toastHeader = document.createElement("div");
+        let img = document.createElement("img");
+        let strong = document.createElement("strong");
+        let small = document.createElement("small");
+        let button = document.createElement("button");
+        let toastBody = document.createElement("div");
+
+        toast.classList.add(...["toast"]);
+        toast.setAttribute("role","alert");
+        toast.setAttribute("aria-live","assertive");
+        toast.setAttribute("aria-atomic","true");
+        toastHeader.classList.add(...["toast-header"]);
+        img.classList.add(...["rounded", "me-2"]);
+        strong.classList.add(...["me-auto"]);
+        button.classList.add(...["btn-close"]);
+        button.setAttribute("type", "button");
+        button.setAttribute("data-bs-dismiss", "toast");
+        button.setAttribute("aria-label", "close");
+        toastBody.classList.add(...["toast-body"]);
+        strong.textContent="Tienda";
+        small.textContent="Hace un segundo";
+        toastBody.textContent=`Se ha agregado el item ${productos[i].nombre}`;
+        document.body.appendChild(toast);
+        toast.appendChild(toastHeader);
+        toast.appendChild(toastBody);
+
+        toast.style=`display: block; position: absolute; top: 0; right: 0;`;
+        [img,strong,small,button].forEach((HTMLElement)=>{
+            toastHeader.appendChild(HTMLElement);
+        });
+        setTimeout(()=>{
+            document.body.removeChild(toast);
+        },2000);
+    }
+
+    eliminarItem() {
+
+    }
+
+
+}
---
+++ index.js
@@ -0,0 +1,242 @@
+const data = [{
+        nombre: "Iphone 14",
+        imagenes: ["/imagenes/iphones/iphone-14/iphone-14-azul-128gb.webp", "/imagenes/iphones/iphone-14/iphone-14-blanco-128gb.webp", "/imagenes/iphones/iphone-14/iphone-14-morado-128gb.webp"],
+        descripcion: "",
+        precio: 120000,
+        categoria: "telefonos",
+        id: 0
+    },
+    {
+        nombre: "Iphone 13",
+        imagenes: ["/imagenes/iphones/iphone-13/iphone-13-azul-128gb.webp", "/imagenes/iphones/iphone-13/iphone-13-blanco-128gb.webp", "/imagenes/iphones/iphone-13/iphone-13-rosa-128gb.webp"],
+        descripcion: "",
+        precio: 120000,
+        categoria: "telefonos",
+        id: 1
+    },
+    {
+        nombre: "Iphone 12",
+        imagenes: ["/imagenes/iphones/iphone-12/iphone-12-azul-128gb.webp", "/imagenes/iphones/iphone-12/iphone-12-blanco-128gb.webp", "/imagenes/iphones/iphone-12/iphone-12-morado-128gb.webp"],
+        descripcion: "",
+        precio: 120000,
+        categoria: "telefonos",
+        id: 2
+    },
+    {
+        nombre: "Iphone 11",
+        imagenes: ["/imagenes/iphones/iphone-11/iphone-11-azul-128gb.webp", "/imagenes/iphones/iphone-11/iphone-11-blanco-128gb.webp", "/imagenes/iphones/iphone-11/iphone-11-morado-128gb.webp"],
+        descripcion: "",
+        precio: 120000,
+        categoria: "telefonos",
+        id: 3
+    },
+    {
+        nombre: "Iphone SE",
+        imagenes: ["/imagenes/iphones/iphone-SE/iphone-se-azul-128gb.webp", "/imagenes/iphones/iphone-SE/iphone-se-blanco-128gb.webp", "/imagenes/iphones/iphone-SE/iphone-se-rojo-128gb.webp"],
+        descripcion: "",
+        precio: 120000,
+        categoria: "telefonos",
+        id: 4
+    },
+    {
+        nombre: "Iphone XR",
+        imagenes: ["/imagenes/iphones/iphone-XR/iphone-xr-negro-128gb.webp", "/imagenes/iphones/iphone-XR/iphone-xr-blanco-128gb.webp", "/imagenes/iphones/iphone-XR/iphone-xr-rojo-128gb.webp"],
+        descripcion: "",
+        precio: 120000,
+        categoria: "telefonos",
+        id: 5
+    },
+    {
+        nombre: "Auriculares con cable",
+        imagenes: ["/imagenes/accesorios/accesorios-auriculares-cable.webp"],
+        descripcion: "",
+        precio: 120000,
+        categoria: "accesorios",
+        id: 6
+    },
+    {
+        nombre: "Auriculares inalambricos",
+        imagenes: ["/imagenes/accesorios/accesorios-auriculares-inalambricos.webp"],
+        descripcion: "",
+        precio: 120000,
+        categoria: "accesorios",
+        id: 7
+    },
+    {
+        nombre: "Cargador con cable",
+        imagenes: ["/imagenes/accesorios/accesorios-cargador-cable.webp"],
+        descripcion: "",
+        precio: 120000,
+        categoria: "accesorios",
+        id: 8
+    },
+    {
+        nombre: "Cargador inalambrico",
+        imagenes: ["/imagenes/accesorios/accesorios-cargador-inalambrico.webp"],
+        descripcion: "",
+        precio: 120000,
+        categoria: "accesorios",
+        id: 9
+    },
+    {
+        nombre: "Usb lightning",
+        imagenes: ["/imagenes/accesorios/accesorios-usb.webp"],
+        descripcion: "",
+        precio: 120000,
+        categoria: "accesorios",
+        id: 10
+    },
+    {
+        nombre: "Funda blanca",
+        imagenes: ["/imagenes/accesorios/accesorios-funda-blanco.webp"],
+        descripcion: "",
+        precio: 120000,
+        categoria: "accesorios",
+        id: 11
+    },
+    {
+        nombre: "Funda Morada",
+        imagenes: ["/imagenes/accesorios/accesorios-funda-morado.webp"],
+        descripcion: "",
+        precio: 120000,
+        categoria: "accesorios",
+        id: 12
+    },
+    {
+        nombre: "Apple watch series 8",
+        imagenes: ["/imagenes/watches/watch-8-azul.webp"],
+        descripcion: "",
+        precio: 120000,
+        categoria: "relojes",
+        id: 13
+    },
+    {
+        nombre: "Apple watch series 7",
+        imagenes: ["/imagenes/watches/watch-7-blanco.webp"],
+        descripcion: "",
+        precio: 120000,
+        categoria: "relojes",
+        id: 14
+    },
+    {
+        nombre: "Apple watch series 6",
+        imagenes: ["/imagenes/watches/watch-6-rojo.webp"],
+        descripcion: "",
+        precio: 120000,
+        categoria: "relojes",
+        id: 15
+    },
+    {
+        nombre: "Apple watch series 5",
+        imagenes: ["/imagenes/watches/watch-5-negro.webp"],
+        descripcion: "",
+        precio: 120000,
+        categoria: "relojes",
+        id: 16
+    },
+    {
+        nombre: "Apple watch series 3",
+        imagenes: ["/imagenes/watches/watch-3-blanco.webp"],
+        descripcion: "",
+        precio: 120000,
+        categoria: "relojes",
+        id: 17
+    },
+    {
+        nombre: "Apple watch series SE",
+        imagenes: ["/imagenes/watches/watch-se-blanco.webp"],
+        descripcion: "",
+        precio: 120000,
+        categoria: "relojes",
+        id: 18
+    }
+];
+
+if (!localStorage.getItem("productos")) {
+    localStorage.setItem("productos", JSON.stringify(data));
+}
+
+let productos = JSON.parse(localStorage.getItem("productos")).sort((a, b) => {
+    return a.id - b.id;
+});
+
+let main = document.querySelector("main");
+let section = document.createElement("section");
+main.appendChild(section);
+
+for (let i = 0; i < productos.length; i++) {
+    let article = document.createElement("article");
+    let div = document.createElement("div");
+    let h3 = document.createElement("h3");
+    let p = document.createElement("p");
+    let spanPrecio = document.createElement("span");
+    let spanCategoria = document.createElement("span");
+    let button = document.createElement("button");
+
+
+    for (let j = 0; j < productos[i].imagenes.length; j++) {
+        let imagen = document.createElement("img");
+        imagen.classList.add(["card-img-top"])
+        article.appendChild(imagen);
+        imagen.src = productos[i].imagenes[j];
+        if (j !== 0) {
+            imagen.style.display = "none";
+        }
+    }
+
+    section.appendChild(article);
+    article.appendChild(div);
+    div.appendChild(h3);
+    div.appendChild(p);
+    div.appendChild(spanPrecio);
+    div.appendChild(spanCategoria);
+    div.appendChild(button);
+
+    main.classList.add(["container"]);
+    section.classList.add(...["row", "justify-content-center"]);
+    article.classList.add(...["card", "m-3", "col-lg-4", "col-md-6", "col-sm-12"]);
+    article.style.width = "18rem";
+    div.classList.add(["card-body"]);
+    h3.classList.add(["card-title"]);
+    p.classList.add(["card-text"]);
+    button.classList.add(...["btn", "btn-primary"]);
+    button.setAttribute("data-id", `${i}`);
+
+
+    h3.innerHTML = productos[i].nombre;
+    p.innerHTML = productos[i].descripcion;
+    spanPrecio.innerHTML = productos[i].precio;
+    spanCategoria.innerHTML = productos[i].categoria;
+    button.innerHTML = "Agregar al carrito";
+}
+
+
+/* Boton agregar carrito  */
+let agregarCarrito = document.querySelectorAll("[data-id]");
+let productoCarrito;
+for (let i = 0; i < agregarCarrito.length; i++) {
+    agregarCarrito[i].addEventListener('click', (e) => {
+        let atributoId = e.target.getAttribute("data-id");
+        productoCarrito = new Carrito(productos[atributoId]);
+        productoCarrito.mensajeCarrito(atributoId);
+    });
+}
+
+/* Creo el html del carrito */
+Carrito.crearHTML();
+
+[document.querySelector("[data-cerrar]"), document.querySelector(".btn-close")].forEach(botones => botones.addEventListener("click", (e) => {
+    Carrito.cerrarCarrito(e);
+}));
+
+document.querySelector("i").addEventListener("click", () => {
+    Carrito.abrirCarrito();
+    Carrito.vacioProductos();
+    Carrito.mostrarProductos();
+    let decrementador = document.querySelectorAll("[data-name='decrementador']");
+    for(let i = 0; i<decrementador.length;i++){
+        decrementador[i].addEventListener("click", (e) => {
+            Carrito.decrementador(e);
+        });
+    }
+});
